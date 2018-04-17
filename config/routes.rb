bc7a9ea27b04910d11e6c875846b7cb7feb118b1
Michelle Machiri <michmach321@gmail.com>
2018-04-17 13:50:02 -0700

    Create new template action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages/home', to: 'pages#home'
+ # get 'pages/home', to: 'pages#home'
   get  'pages/about', to: 'pages#about'
   root 'pages#home'
+# a new article path, a post, to create articles. a patch to edit articles,  edit, show, delete and index path. we get that from this one line
+  resources :articles
+
 end
