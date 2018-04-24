4f91481c2aeec2f70b0b78868cc512668968cf65
Michelle Machiri <michmach321@gmail.com>
2018-04-24 14:27:35 -0700

    Styled error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 def index
   #can be called anything.
   @articles= Article.all
-end
+  end
   def new
       @article = Article.new
   end
@@ -17,7 +17,7 @@ def create
   @article = Article.new(article_params)
 
   if @article.save
-    flash[:notice] = "Article was successfully created!"
+    flash[:success] = "Article was successfully created!"
     redirect_to article_path(@article)
   else
     render 'new'
@@ -32,14 +32,14 @@ def destroy
 
 
   @article.destroy
-  flash[:notice] = "Articles was sucessfully destroyed"
+  flash[:danger] = "Articles was sucessfully destroyed"
   redirect_to articles_path
 end
 
 def update
 
   if @article.update(article_params)
-    flash[:notice] = "Article was successfully updated"
+    flash[:success] = "Article was successfully updated"
     redirect_to article_path(@article)
   else
     render 'edit'
